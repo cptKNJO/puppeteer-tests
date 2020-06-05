40811e75971e97cb44b9e6811cba98e2942b53a2
Kazi Noshin J. O <kazinoshin.jo@gmail.com>
2020-06-05 21:16:01 +0600

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    document: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'no-underscore-dangle': 'off',
+  },
+};
